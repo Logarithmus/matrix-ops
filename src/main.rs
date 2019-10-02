92778c6211ea4d691c43d4b9aaa69fe6f236e01d
Logarithmus <logarithmus.dev@gmail.com>
2019-10-02 13:54:18 +0300

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,15 @@ mod matrix;
 use matrix::Matrix;
 
 fn task1() {
+    println!("Task 1: in-place matrix tranpose");
     let mut mat = Matrix::<i8>::rand((3, 3));
     println!("A =\n{}", mat);
     mat.transpose_inplace();
-    println!("A трансп. =\n{}", mat);
+    println!("A transposed =\n{}", mat);
 }
 
 fn task2() {
+    println!("Task 2: matrix sum");
     let mat1 = Matrix::<i8>::rand_range((3, 3), (-50, 50));
     let mat2 = Matrix::<i8>::rand_range((3, 3), (-50, 50));
     println!("A =\n{}", mat1);
@@ -17,36 +19,10 @@ fn task2() {
     println!("A + B =\n{}", &mat1 + &mat2);
 }
 
-/*fn task3() {
-    let mat1 = Matrix::<i16>::rand_range((3, 5), (-50, 50));
-    let mat2 = Matrix::<i16>::rand_range((5, 2), (-50, 50));
-    println!("A =\n{}", mat1);
-    println!("B =\n{}", mat2);
-    let product = matrix::mult_naive(&mat1, &mat2);
-    println!("A * B =\n{}", product);
-}
-
-fn task4() {
-    let mat1 = Matrix::<i16>::rand_range((3, 5), (-50, 50));
-    let mat2 = Matrix::<i16>::rand_range((5, 2), (-50, 50));
-    println!("A =\n{}", mat1);
-    println!("B =\n{}", mat2);
-    let product = matrix::mult_winograd(&mat1, &mat2);
-    println!("A * B =\n{}", product);
-}
-
-fn task5() {
-    let mat1 = Matrix::<i16>::rand_range((2, 2), (-50, 50));
-    let mat2 = Matrix::<i16>::rand_range((2, 2), (-50, 50));
-    println!("A =\n{}", mat1);
-    println!("B =\n{}", mat2);
-    let product = matrix::mult_strassen(&mat1, &mat2);
-    println!("A * B =\n{}", product);
-}*/
-
-fn task_mult_all() {
-    let mat1 = Matrix::<i16>::rand_range((2, 2), (-50, 50));
-    let mat2 = Matrix::<i16>::rand_range((2, 2), (-50, 50));
+fn tasks_3_4_5() {
+    println!("Tasks 3, 4, 5: matrix multiplication");
+    let mat1 = Matrix::<i16>::rand_range((2, 5), (-50, 50));
+    let mat2 = Matrix::<i16>::rand_range((5, 3), (-50, 50));
     println!("A =\n{}", mat1);
     println!("B =\n{}", mat2);
     let product = matrix::mult_naive(&mat1, &mat2);
@@ -58,10 +34,9 @@ fn task_mult_all() {
 }
 
 fn main() {
-    println!("Task 1");
     task1();
-    println!("\nTask 2");
+    println!("----------------------------");
     task2();
-    println!("\nTasks 3, 4, 5");
-    task_mult_all();
+    println!("----------------------------");
+    tasks_3_4_5();
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,10 @@
+use std::borrow::Borrow;
 use core::ops::{Add, Sub, AddAssign, SubAssign, Index, IndexMut, Mul};
 use rand::distributions::{uniform::SampleUniform, Distribution, Standard};
 use rand::Rng;
 use std::default::Default;
 use std::fmt::{self, Debug, Display, Formatter};
-use std::iter::Sum;
+use std::iter::{self, Sum};
 
 pub struct Matrix<T> {
     data: Vec<T>,
@@ -26,10 +27,17 @@ impl<T> IndexMut<(usize, usize)> for Matrix<T> {
     }
 }
 
-
 impl<'a, T: Add<Output = T> + Copy> Add for &'a Matrix<T> {
     type Output = Matrix<T>;
     fn add(self, other: Self) -> Self::Output {
+        let iter12 = self.data.iter().zip(other.data.iter());
+        Matrix::with_data(iter12.map(|(&a, &b)| a + b).collect(), (self.rows, self.cols)) 
+    }
+}
+
+impl<'a, T: Add<Output = T> + Copy> Add<&'a Matrix<T>> for Matrix<T> {
+    type Output = Matrix<T>;
+    fn add(self, other: &'a Self) -> Self::Output {
         let iter12 = self.data.iter().zip(other.data.iter());
         Matrix::with_data(iter12.map(|(&a, &b)| a + b).collect(), (self.rows, self.cols)) 
     }
@@ -43,9 +51,16 @@ impl<'a, T: Sub<Output = T> + Copy> Sub for &'a Matrix<T> {
     }
 }
 
+impl<'a, T: Sub<Output = T> + Copy> Sub<&'a Matrix<T>> for Matrix<T> {
+    type Output = Matrix<T>;
+    fn sub(self, other: &'a Self) -> Self::Output {
+        let iter12 = self.data.iter().zip(other.data.iter());
+        Matrix::with_data(iter12.map(|(&a, &b)| a - b).collect(), (self.rows, self.cols)) 
+    }
+}
+
 impl<T> Display for Matrix<T>
-where
-    T: Debug,
+where T: Debug,
 {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         for row in self.data.chunks(self.cols) {
@@ -58,9 +73,7 @@ where
 
 impl<T> Matrix<T> {
     pub fn new((rows, cols): (usize, usize)) -> Matrix<T>
-    where
-        T: Clone + Default,
-    {
+    where T: Clone + Default {
         let data = vec![T::default(); rows * cols];
         Matrix { data, rows, cols }
     }
@@ -69,9 +82,14 @@ impl<T> Matrix<T> {
         Matrix { data, rows, cols }
     }
 
+    pub fn from(slice: &[&[T]]) -> Matrix<T>
+    where T: Clone {
+        let data = slice.iter().map(|row| row.iter()).flatten().cloned().collect();
+        Matrix::with_data(data, (slice.len(), slice[0].len()))
+    }
+
     pub fn from_quadrants(q: &[[Matrix<T>; 2]; 2]) -> Matrix<T>
-    where T: Copy
-    {
+    where T: Clone {
         let (rows, cols) = (q[0][0].rows, q[0][0].cols);
         let row_iters = 
         (
@@ -82,23 +100,19 @@ impl<T> Matrix<T> {
         );
         let q0_iter = row_iters.0.zip(row_iters.1).map(|row| row.0.iter().chain(row.1.iter())).flatten();
         let q1_iter = row_iters.2.zip(row_iters.3).map(|row| row.0.iter().chain(row.1.iter())).flatten();
-        let data: Vec<T> = q0_iter.chain(q1_iter).copied().collect();
+        let data: Vec<T> = q0_iter.chain(q1_iter).cloned().collect();
         Matrix::with_data(data, (rows * 2, cols * 2)) 
     }
 
     pub fn rand((rows, cols): (usize, usize)) -> Matrix<T>
-    where
-        Standard: Distribution<T>,
-    {
+    where Standard: Distribution<T> {
         let mut rng = rand::thread_rng();
         let data = (0..rows * cols).map(|_| rng.gen()).collect();
         Matrix { data, rows, cols }
     }
 
     pub fn rand_range((rows, cols): (usize, usize), (low, high): (T, T)) -> Matrix<T>
-    where
-        T: SampleUniform + Copy,
-    {
+    where T: SampleUniform + Copy {
         let mut rng = rand::thread_rng();
         let data = (0..rows * cols).map(|_| rng.gen_range(low, high)).collect();
         Matrix { data, rows, cols }
@@ -114,32 +128,46 @@ impl<T> Matrix<T> {
     }
 
     pub fn quadrants(&self) -> [[Matrix<T>; 2]; 2]
-    where
-        T: Copy
-    {
+    where T: Clone {
         let (rows, cols, data) = (self.rows, self.cols, &self.data);
-        let top_left = data.chunks(cols).take(rows / 2)
-            .map(|row| &row[..(cols / 2)])
-            .flatten();
-        let top_right = data.chunks(cols).take(rows / 2)
-            .map(|row| &row[(cols / 2)..])
-            .flatten();
-        let bottom_left = data.chunks(cols).skip(rows / 2)
-            .map(|row| &row[..(cols / 2)])
-            .flatten();
-        let bottom_right = data.chunks(cols).skip(rows / 2)
-            .map(|row| &row[(cols / 2)..])
-            .flatten();
+        let top_left = data.chunks(cols).take(rows / 2).map(|row| &row[..(cols / 2)]);
+        let top_right = data.chunks(cols).take(rows / 2).map(|row| &row[(cols / 2)..]);
+        let bottom_left = data.chunks(cols).skip(rows / 2).map(|row| &row[..(cols / 2)]);
+        let bottom_right = data.chunks(cols).skip(rows / 2).map(|row| &row[(cols / 2)..]);
         [
             [
-                Matrix::with_data(top_left.copied().collect(), (rows / 2, cols / 2)),
-                Matrix::with_data(top_right.copied().collect(), (rows / 2, cols / 2)),
+                Matrix::with_data(top_left.flatten().cloned().collect(), (rows / 2, cols / 2)),
+                Matrix::with_data(top_right.flatten().cloned().collect(), (rows / 2, cols / 2)),
             ],
             [
-                Matrix::with_data(bottom_left.copied().collect(), (rows / 2, cols / 2)),
-                Matrix::with_data(bottom_right.copied().collect(), (rows / 2, cols / 2)),
+                Matrix::with_data(bottom_left.flatten().cloned().collect(), (rows / 2, cols / 2)),
+                Matrix::with_data(bottom_right.flatten().cloned().collect(), (rows / 2, cols / 2)),
             ],
         ]
+    }
+
+    pub fn padded(&self, (rows, cols): (usize, usize)) -> Matrix<T>
+    where T: Clone + Default {
+        let (new_rows, new_cols) = (rows, cols);
+        let Matrix {rows, cols, data} = self;
+        let rows_iter = data.chunks(*cols).map(|row| row.iter().cloned());
+        let zeroes_iter = iter::repeat(iter::repeat(T::default()).take(new_cols - cols)).take(*rows);
+        let padded_iter = rows_iter.zip(zeroes_iter).map(|row| row.0.chain(row.1)).flatten();
+        let zeroes_iter = iter::repeat(iter::repeat(T::default()).take(new_cols)).take(new_rows - rows);
+        let padded_iter = padded_iter.chain(zeroes_iter.flatten());
+        Matrix::with_data(padded_iter.collect(), (new_rows, new_cols))
+    }
+
+    pub fn padded_to_pow2(&self) -> Matrix<T>
+    where T: Clone + Default {
+        let max_pow2 = std::cmp::max(self.rows.next_power_of_two(), self.cols.next_power_of_two());
+        self.padded((max_pow2, max_pow2))
+    }
+
+    pub fn cropped(&self, (rows, cols): (usize, usize)) -> Matrix<T>
+    where T: Clone {
+        let cropped_iter = self.data.chunks(self.cols).take(rows).map(|row| &row[..cols]).flatten();
+        Matrix::with_data(cropped_iter.cloned().collect(), (rows, cols))
     }
 }
 
@@ -147,7 +175,6 @@ pub fn mult_naive<T>(mat1: &Matrix<T>, mat2: &Matrix<T>) -> Matrix<T>
 where
     T: Mul<Output = T> + Add<Output = T> + AddAssign + Default + Copy,
 {
-    assert_eq!(mat1.cols, mat2.rows);
     let mut product = Matrix::new((mat1.rows, mat2.cols));
     for row in 0..mat1.rows {
         for k in 0..mat1.cols {
@@ -201,32 +228,44 @@ where
     product
 }
 
-pub fn mult_strassen<T>(mat1: &Matrix<T>, mat2: &Matrix<T>) -> Matrix<T>
+fn mult_strassen_pow2<T>(mat1: &Matrix<T>, mat2: &Matrix<T>) -> Matrix<T>
 where
     T: Mul<Output = T> + Add<Output = T> + Sub<Output = T> + Copy,
 {
+    let (mat1, mat2) = (mat1.borrow(), mat2.borrow());
     if (mat1.rows == 1) && (mat1.cols == 1) {
         return Matrix::with_data(vec![mat1[(0, 0)] * mat2[(0, 0)]; 1], (1, 1));
     }
+
     let (a, b) = (mat1.quadrants(), mat2.quadrants());
     let m = [
-                mult_strassen(&(&a[0][0] + &a[1][1]), &(&b[0][0] + &b[1][1])),
-                mult_strassen(&(&a[1][0] + &a[1][1]), &b[0][0]),
-                mult_strassen(&a[0][0], &(&b[0][1] - &b[1][1])),
-                mult_strassen(&a[1][1], &(&b[1][0] - &b[0][0])),
-                mult_strassen(&(&a[0][0] + &a[0][1]), &b[1][1]),
-                mult_strassen(&(&a[1][0] - &a[0][0]), &(&b[0][0] + &b[0][1])),
-                mult_strassen(&(&a[0][1] - &a[1][1]), &(&b[1][0] + &b[1][1]))
+                mult_strassen_pow2(&(&a[0][0] + &a[1][1]), &(&b[0][0] + &b[1][1])),
+                mult_strassen_pow2(&(&a[1][0] + &a[1][1]), &b[0][0]),
+                mult_strassen_pow2(&a[0][0], &(&b[0][1] - &b[1][1])),
+                mult_strassen_pow2(&a[1][1], &(&b[1][0] - &b[0][0])),
+                mult_strassen_pow2(&(&a[0][0] + &a[0][1]), &b[1][1]),
+                mult_strassen_pow2(&(&a[1][0] - &a[0][0]), &(&b[0][0] + &b[0][1])),
+                mult_strassen_pow2(&(&a[0][1] - &a[1][1]), &(&b[1][0] + &b[1][1]))
             ];
     let c = [
                 [
-                    &(&(&m[0] + &m[3]) - &m[4]) + &m[6],
+                    &m[0] + &m[3] - &m[4] + &m[6],
                     &m[2] + &m[4],
                 ],
                 [
                     &m[1] + &m[3],
-                    &(&(&m[0] - &m[1]) + &m[2]) + &m[5],
+                    &m[0] - &m[1] + &m[2] + &m[5],
                 ]
             ];
     Matrix::from_quadrants(&c)
 }
+
+pub fn mult_strassen<T>(mat1: &Matrix<T>, mat2: &Matrix<T>) -> Matrix<T>
+where
+    T: Mul<Output = T> + Add<Output = T> + Sub<Output = T> + Copy + Clone + Default,
+{
+    assert_eq!(mat1.cols, mat2.rows);
+    mult_strassen_pow2(&mat1.padded_to_pow2(), &mat2.padded_to_pow2())
+        .cropped((mat1.rows, mat2.cols))
+}
+
